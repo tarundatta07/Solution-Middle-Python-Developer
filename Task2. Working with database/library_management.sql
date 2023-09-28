bd0dcd3b8366ff8d561dcc94f2fa39b4772d0e75
Tarun Datta <tarundattacse@gmail.com>
2023-09-28 22:12:00 +0600

    Task 2 Added.

---
+++ Task2. Working with database/library_management.sql
@@ -0,0 +1,142 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Sep 28, 2023 at 06:03 PM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.2.4
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `library_management`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `authors`
+--
+
+CREATE TABLE `authors` (
+  `author_id` int(11) NOT NULL,
+  `author_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `books`
+--
+
+CREATE TABLE `books` (
+  `book_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `author_id` int(11) DEFAULT NULL,
+  `genre_id` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `genres`
+--
+
+CREATE TABLE `genres` (
+  `genre_id` int(11) NOT NULL,
+  `genre_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `patrons`
+--
+
+CREATE TABLE `patrons` (
+  `patron_id` int(11) NOT NULL,
+  `patron_name` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `authors`
+--
+ALTER TABLE `authors`
+  ADD PRIMARY KEY (`author_id`);
+
+--
+-- Indexes for table `books`
+--
+ALTER TABLE `books`
+  ADD PRIMARY KEY (`book_id`),
+  ADD KEY `author_id` (`author_id`),
+  ADD KEY `genre_id` (`genre_id`);
+
+--
+-- Indexes for table `genres`
+--
+ALTER TABLE `genres`
+  ADD PRIMARY KEY (`genre_id`);
+
+--
+-- Indexes for table `patrons`
+--
+ALTER TABLE `patrons`
+  ADD PRIMARY KEY (`patron_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `authors`
+--
+ALTER TABLE `authors`
+  MODIFY `author_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `books`
+--
+ALTER TABLE `books`
+  MODIFY `book_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `genres`
+--
+ALTER TABLE `genres`
+  MODIFY `genre_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `patrons`
+--
+ALTER TABLE `patrons`
+  MODIFY `patron_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `books`
+--
+ALTER TABLE `books`
+  ADD CONSTRAINT `books_ibfk_1` FOREIGN KEY (`author_id`) REFERENCES `authors` (`author_id`),
+  ADD CONSTRAINT `books_ibfk_2` FOREIGN KEY (`genre_id`) REFERENCES `genres` (`genre_id`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ Task2. Working with database/scripting_crud_operation.py
@@ -0,0 +1,97 @@
+import mysql.connector
+
+# Connect to the MySQL database
+db_connection = mysql.connector.connect(
+	host="localhost",
+	user="root",
+	password="",
+	database="library_management"
+)
+
+# Create a cursor object to interact with the database
+cursor = db_connection.cursor()
+
+
+# CREATE - Add a new author
+def add_author(authors_name):
+	query = "INSERT INTO authors (author_name) VALUES (%s)"
+	values = [(author,) for author in authors_name]
+	cursor.executemany(query, values)
+	db_connection.commit()
+	return cursor.lastrowid
+
+
+# CREATE - Add a new genre
+def add_genre(genres_name):
+	query = "INSERT INTO genres (genre_name) VALUES (%s)"
+	values = [(genre,) for genre in genres_name]
+	cursor.executemany(query, values)
+	db_connection.commit()
+	return cursor.lastrowid
+
+
+# CREATE - Add a new book
+def add_book(list_of_books):
+	query = "INSERT INTO books (title, author_id, genre_id) VALUES (%s, %s, %s)"
+	values = list_of_books
+	cursor.executemany(query, values)
+	db_connection.commit()
+	return cursor.lastrowid
+
+
+# CREATE - Add a new patron
+def add_patron(patrons_name):
+	query = "INSERT INTO patrons (patron_name) VALUES (%s)"
+	values = [(patron,) for patron in patrons_name]
+	cursor.executemany(query, values)
+	db_connection.commit()
+	return cursor.lastrowid
+
+
+# READ - Retrieve all books
+def get_all_books():
+	query = "SELECT books.book_id, books.title, authors.author_name, genres.genre_name FROM books \
+             INNER JOIN authors ON books.author_id = authors.author_id \
+             INNER JOIN genres ON books.genre_id = genres.genre_id"
+	cursor.execute(query)
+	all_books = cursor.fetchall()
+	return all_books
+
+
+# UPDATE - Update book information
+def update_book(book_id, new_title, new_author_id, new_genre_id):
+	query = "UPDATE books SET title = %s, author_id = %s, genre_id = %s WHERE book_id = %s"
+	values = (new_title, new_author_id, new_genre_id, book_id)
+	cursor.execute(query, values)
+	db_connection.commit()
+
+
+# DELETE - Delete a book
+def delete_book(book_id):
+	query = "DELETE FROM books WHERE book_id = %s"
+	cursor.execute(query, (book_id,))
+	db_connection.commit()
+
+
+# Example usage
+authors_list = ["David Smith", "Harry kane", "Robertson ", "Sam Curran"]
+genre_list = ["Literary Fiction", "Science Fiction", "Historical Fiction ", "Mystery"]
+patrons_list = ["Tom", "Jerry", "Alex ", "Peter"]
+books_list = [('Programming Language', 1, 1), ('Sharloks Homes', 2, 2), ('World War', 3, 3), ('Amazon', 4, 4)]
+
+author_id = add_author(authors_list)
+genre_id = add_genre(genre_list)
+book_id = add_book(books_list)
+patron_id = add_patron(patrons_list)
+
+print("All Books:")
+books = get_all_books()
+for book in books:
+	print(f"Book ID: {book[0]}, Title: {book[1]}, Author: {book[2]}, Genre: {book[3]}")
+
+update_book(2, "Python", 1, 3)
+delete_book(4)
+
+# Close the database connection
+cursor.close()
+db_connection.close()
